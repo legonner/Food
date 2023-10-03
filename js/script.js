df173d4f8d3a3c1c6960e9db4a2f14a41b9feeb3
Maksym <legonner@gmail.com>
2023-10-03 09:39:51 +0300

    first commit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     // Tabs
+    
     const tabs = document.querySelectorAll('.tabheader__item'),
         tabsContent = document.querySelectorAll('.tabcontent'),
         tabsParent = document.querySelector('.tabheader__items');
